e13775bc5b72c73ac306297e3e81638166ec1811
Nur Mohsin <Nur Mohsin>
2019-11-20 00:55:54 +0600

    login logout

--- MovieRater/frontend/web/src/app/auth/auth.component.ts
+++ MovieRater/frontend/web/src/app/auth/auth.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
+import { ApiService } from "../_service/api.service";
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+
+interface TokenObj {
+  token: string
+}
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
+
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  });
+
+  constructor(
+    private apiService: ApiService,
+    private cookieService: CookieService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const mrToken = this.cookieService.get('mr-token');
+    if(mrToken){
+      this.router.navigate(['movies']);
+    }
+  }
+
+  saveForm(){
+    this.apiService.loginUser(this.loginForm.value).subscribe(
+      (result: TokenObj) => {
+        this.cookieService.set('mr-token', result.token);
+        this.router.navigate(['movies']);
+      },
+      error => console.log(error)
+      
+    )
   }
 
 }
--- MovieRater/frontend/web/src/app/main/movie-form/movie-form.component.ts
+++ MovieRater/frontend/web/src/app/main/movie-form/movie-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Movie } from '../../models/Movie';
 import { FormGroup, FormControl } from "@angular/forms";
+import { ApiService } from "../../_service/api.service";
 
 @Component({
   selector: 'app-movie-form',
@@ -10,21 +11,49 @@ import { FormGroup, FormControl } from "@angular/forms";
 
 export class MovieFormComponent implements OnInit {
 
-  @Input() movie: Movie;
+  movieForm;
+  id = null;
 
-  movieForm = new FormGroup({
-    title: new FormControl,
-    description: new FormControl
-  });
+  @Output() createdMovie = new EventEmitter<Movie>();
+  @Output() updatedMovie = new EventEmitter<Movie>()
 
-  constructor() { }
+  // @Input() movie: Movie;
+  @Input() set movie(val: Movie){
+    this.id = val.id;
+    this.movieForm = new FormGroup({
+      title: new FormControl(val.title),
+      description: new FormControl(val.description)
+    });
+  }
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
   }
 
-  saveForm() {
-    console.log(this.movieForm.value);
-    
+  formDisabled() {
+    if (this.movieForm.value.title.length && this.movieForm.value.description.length){
+      return false;
+    }else {
+      return true;
+    }
   }
 
+  saveForm() {
+    if(this.id){
+      this.apiService.updateMovie(this.id,
+        this.movieForm.value.title, this.movieForm.value.description).subscribe(
+          (result: Movie) => this.updatedMovie.emit(result),
+          error => console.log(error)
+        );
+    }else{
+      this.apiService.createMovie(
+        this.movieForm.value.title, this.movieForm.value.description).subscribe(
+          (result: Movie) => this.createdMovie.emit(result),
+          error => console.log(error)
+        );
+    } 
+  }
 }
