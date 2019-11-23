f848b6ef6a408c2d22f4af846ef77166fee8a615
Nur Mohsin <Nur Mohsin>
2019-11-23 14:10:28 +0600

    mosh angular new product create

---
+++ Mosh-Angular/oshop/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBRcZ1vpDUplrFATRGQcsXA1MC8H5InMXQ",
+    authDomain: "oshop-19ab3.firebaseapp.com",
+    databaseURL: "https://oshop-19ab3.firebaseio.com",
+    projectId: "oshop-19ab3",
+    storageBucket: "oshop-19ab3.appspot.com",
+    messagingSenderId: "439509985704",
+    appId: "1:439509985704:web:d9891b0f3775ea2b46559d",
+    measurementId: "G-ZGQXXTM6D0"
+
+  }
+};
+
---
+++ Mosh-Angular/oshop/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBRcZ1vpDUplrFATRGQcsXA1MC8H5InMXQ",
+    authDomain: "oshop-19ab3.firebaseapp.com",
+    databaseURL: "https://oshop-19ab3.firebaseio.com",
+    projectId: "oshop-19ab3",
+    storageBucket: "oshop-19ab3.appspot.com",
+    messagingSenderId: "439509985704",
+    appId: "1:439509985704:web:d9891b0f3775ea2b46559d",
+    measurementId: "G-ZGQXXTM6D0"
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
