d791f6a9a544a04a909ac444de456a0ca9887e22
Mohsin <follow.mohsin@gmail.com>
2019-12-01 18:11:56 +0600

    user modification

--- MovieRater/api/core/views.py
+++ MovieRater/api/core/views.py
@@ -6,12 +6,26 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Post
 from .serializers import UserSerializer, PostSerializer
+import re
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+
+    def create(self, request, *args, **kwargs):
+        if 'username' in request.data:
+            phone_re = "(^([+]{1}[8]{2}|0088|88)?(01){1}[3-9]{1}\d{8})$"
+            if re.search(phone_re, request.data['username']):
+                response = {'message': request.data['username']}
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                response = {'message': 'Phone number is not valid.'}
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            response = {'message': 'Username field is mendatory'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostViewSet(viewsets.ModelViewSet):
--- MovieRater/backend/api/views.py
+++ MovieRater/backend/api/views.py
@@ -7,10 +7,12 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RattingSerializer, UserSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
@@ -26,34 +28,38 @@ class MovieViewSet(viewsets.ModelViewSet):
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
             user = request.user
-            
+
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
                 rating.stars = stars
                 rating.save()
                 serializer = RattingSerializer(rating, many=False)
-                response = {'message': 'Rating updated', 'result': serializer.data}
+                response = {'message': 'Rating updated',
+                            'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
             except:
-                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                rating = Rating.objects.create(
+                    user=user, movie=movie, stars=stars)
                 rating.save()
                 serializer = RattingSerializer(rating, many=False)
-                response = {'message': 'Rating created', 'result': serializer.data}
+                response = {'message': 'Rating created',
+                            'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
         else:
             response = {'message': 'You need to pass stars!'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RattingSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
-    
+
     def update(self, request, *args, **kwargs):
         response = {'message': 'You cant update rating like this'}
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def create(self, request, *args, **kwargs):
         response = {'message': 'You cant create rating like this'}
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
